dbd1d1e3568bdcf8c8dfed233c5449fc3fa22b5b
Mehdi Maaref <mehdimaaref@live.fr>
2024-02-23 00:50:58 +0100

    [ENH] :sparkles: First commit for app-spring

---
+++ src/test/java/bootstrap/example/com/appspring/AppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package bootstrap.example.com.appspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
