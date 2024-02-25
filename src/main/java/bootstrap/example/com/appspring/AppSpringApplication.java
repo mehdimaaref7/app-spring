5be560782fead016c4522c4c5ed92b18874b4d68
Mehdi Maaref <mehdimaaref@live.fr>
2024-02-25 21:56:27 +0100

    [ENH] :sparkles: Add Entities & Persistance with H2 db

--- src/main/java/bootstrap/example/com/appspring/AppSpringApplication.java
+++ src/main/java/bootstrap/example/com/appspring/AppSpringApplication.java
@@ -1,7 +1,11 @@
 package bootstrap.example.com.appspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("bootstrap.example.com.appspring")
+@EntityScan("bootstrap.example.com.appspring")
 @SpringBootApplication
 public class AppSpringApplication {
 
---
+++ src/main/java/bootstrap/example/com/appspring/datamodel/Book.java
@@ -0,0 +1,19 @@
+package bootstrap.example.com.appspring.datamodel;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+
+}
